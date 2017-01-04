954620611b728fffd464859130e4385daaa8181f
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-01-04 17:40:32 -0600

    turn off uglify because it's too slow

--- webpack.config.js
+++ webpack.config.js
@@ -31,9 +31,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
